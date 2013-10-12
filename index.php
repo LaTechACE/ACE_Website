436c7e447fddca84579b9c89026f69631e7f8dc8
Gustave Michel III <gus3@michelfamily.org>
2013-10-12 14:47:16 -0500

    Nothing much

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>The Association of Cyber Engineers @ Louisiana Tech</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="navigation">
+            
+        </div>
+        <div id="header">
+            
+        </div>
+        <div id="body">
+            
+        </div>
+        <div id="footer">
+            
+        </div>
     </body>
 </html>
