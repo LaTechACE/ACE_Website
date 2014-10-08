a2dfdd3f533c0c9af524c4fdac9849009c183e43
Gustave Abel Michel III <gus3@michelfamily.org>
2014-10-07 21:01:33 -0500

    Added Officer Bio Profiles

--- pages/officers.php
+++ pages/officers.php
@@ -1,27 +1,82 @@
 <?php
-
+    $officers = array
+    (
+        'seansemple' => array
+        (
+            'name' => 'Sean Semple',
+            'position' => 'President',
+            'email' => '',
+            'bio' => ''
+        ),
+        'gustavemichel' => array
+        (
+            'name' => 'Gustave Michel',
+            'position' => 'Vice President',
+            'email' => 'gam017@latech.edu',
+            'bio' => ''
+        ),
+        'anthonyagee' => array
+        (
+            'name' => 'Anthony Agee',
+            'position' => 'Secretary',
+            'email' => '',
+            'bio' => ''
+        ),
+        'holdenrose' => array
+        (
+            'name' => 'Holden Rose',
+            'position' => 'Treasurer',
+            'email' => '',
+            'bio' => ''
+        ),
+        'brandonbryant' => array
+        (
+            'name' => 'Brandon Bryant',
+            'position' => 'Historian',
+            'email' => '',
+            'bio' => ''
+        ),
+        'ashleighmulamula' => array
+        (
+            'name' => 'Ashleigh Mulamula',
+            'position' => 'Social Media Chair',
+            'email' => '',
+            'bio' => ''
+        )
+        
+        /*
+        '' => array
+        (
+            'name' => '',
+            'position' => '',
+            'email' => '',
+            'bio' => ''
+        ),
+        */
+        
+    );
 ?>
 
 <div id="body_main">
-    <div>
-        Actual Bios will be added here later
-    </div>
-    <div>
-        Sean Semple: President
-    </div>
-    <div>
-        Gustave Michel: Vice President
-    </div>
-    <div>
-        Tony: Secretary
-    </div>
-    <div>
-        Holden Rose: Treasurer
-    </div>
-    <div>
-        Brandon: Historian
-    </div>
-    <div>
-        Ashleigh Mulamula: Social Media Chair
-    </div>
+    <?php
+    foreach($officers as $key => $info)
+    {
+        echo '<div class="bio">';
+            echo '<div class="picture">';
+                echo '<img src="./resources/officers/'.$key.'.jpg" alt="Picture of '.$info['name'].'">';
+            echo '</div>';
+            echo '<div class="info">';
+                echo '<div class="name">';
+                    echo $info['name'];
+                echo '</div>';
+                echo '<div class="position">';
+                    echo $info['position'];
+                echo '</div>';
+                echo '<div class="email">';
+                    echo '<a href="mailto:'.$info['email'].'">'.$info['email'].'</a>';
+                echo '</div>';
+            echo '</div>';
+        echo '</div>'."\n";
+    }
+    ?>
 </div>
