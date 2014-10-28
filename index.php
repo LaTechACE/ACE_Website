d5e473b8d3b2ca7b29ab803b38f7024a23853255
Gustave Abel Michel III <gus3@michelfamily.org>
2014-10-28 09:42:49 -0500

    Changed bs directory to bootstrap for clarification. Changed index.php to match

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 	<head>
 		<title>The Association of Cyber Engineers @ Louisiana Tech</title>
 		
+		<!-- Bootstrap 3 and Bootstrap Theme CSS  -->
+		<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="./bootstrap/css/bootstrap-theme.min.css" />
+		
 		<link rel="stylesheet" href="./resources/styles.css" />
-		<link rel="stylesheet" href="./bs/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="./bs/css/bootstrap-theme.min.css" />		
+		
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -15,15 +18,15 @@
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-						<!--<span class="sr-only">Toggle navigation</span>-->
+						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
 					<a class="navbar-brand" href="./index.php"><img src="./resources/ACELogo.png" /></a>
 				</div>
-				<div class="collapse navbar-collapse pull-right">
-					<ul class="nav navbar-nav">
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
 						<?php
 						$links = array
 						(
@@ -69,14 +72,57 @@ include "./pages/".$_GET['p'].".php";
 		}
 		?>
 		</div>
+		<!--FOOTER-->
+		<footer>
+			<div class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-3 col-xs-8">
+							<a href="http://www.latech.edu/"><img src="./resources/tech_new_logo_wm_web.jpg" /></a>
+						</div>
+						<div class="col-sm-9 col-xs-4">
+							<div class="row">
+								<?php
+									$social_media = array
+									(
+										//'facebook' => 'https://www.facebook.com/TigerRobotics3946',
+										'twitter' => 'https://twitter.com/#!/LaTechACE',
+										//'googleplus' => 'https://plus.google.com/118198635080612986989/posts',
+										//'youtube' => 'https://www.youtube.com/user/slidellrobotics'
+									);
+									foreach($social_media as $picture => $url)
+									{
+										echo '<div class="text-center col-sm-3 col-md-1">';
+										echo '<a href="'.$url.'"><img src="./resources/social/'.$picture.'.png" /></a>';
+										echo '</div>';
+									}
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+                <!-- jQuery and Bootstrap Javascript  -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                <script src="./bootstrap/js/bootstrap.min.js"></script>
+		
+                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                <!--[if lt IE 9]>
+                        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                <![endif]-->
+		
+                <!-- Google Analytics  -->
+                <script>
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="./bs/js/bootstrap.min.js"></script>
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+                        ga('create', 'UA-55833900-1', 'auto');
+                        ga('send', 'pageview');
+                </script>
 	</body>
 </html>
--- pages/home.php
+++ pages/home.php
@@ -1,16 +1,35 @@
 <?php
-    
-?>
 
+?>
 <div class="row">
 	<div class="col-md-9">
-		<iframe 
-			src="https://www.google.com/calendar/embed?src=latechace%40gmail.com&amp;ctz=America/Chicago"
-			style="border: 0; width: 100%; min-height: 600px; height: 100%; "
-			frameborder="0"
-			scrolling="no"
-		>
-		</iframe>
+		<div class="hidden-sm hidden-xs">
+			<iframe class="calendar" src="https://www.google.com/calendar/embed?
+					showPrint=1&amp;
+					showTabs=1&amp;
+					showCalendars=0&amp;
+					wkst=1&amp;
+					src=latechace%40gmail.com&amp;
+					ctz=America%2FChicago"
+				style="border: 0; width: 100%; min-height: 600px; height: 100%;"
+				frameborder="0"
+				scrolling="no">
+			</iframe>
+		</div>
+		<div class="visible-xs visible-sm">
+			<iframe class="upcomingEvents" src="https://www.google.com/calendar/embed?
+					showPrint=0&amp;
+					showTabs=0&amp;
+					showCalendars=0&amp;
+					mode=AGENDA&amp;
+					wkst=1&amp;
+					src=latechace%40gmail.com&amp;
+					ctz=America%2FChicago"
+				style="border: 0; width: 100%; min-height: 600px; height: 100%;"
+				frameborder="0"
+				scrolling="no">
+			</iframe>
+		</div>
 	</div>
 	<div class="col-md-3">
 		<a class="twitter-timeline"  href="https://twitter.com/LaTechACE" data-widget-id="511954506985115648">Tweets by @LaTechACE</a>
